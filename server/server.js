43fa62bfe61c059336e13f4de9dce29f96699220
gourav <gauravswain007@gmailcom>
2026-02-17 17:34:06 +0530

    server fixing

--- server/server.js
+++ server/server.js
@@ -14,5 +14,5 @@ app.use(express.json());
 app.use("/api/generate", generateRoute);
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
