56569771076316b27304c93c25d9538b0bd23a8d
gourav <gauravswain007@gmailcom>
2026-02-17 18:00:00 +0530

    fixing again

--- server/server.js
+++ server/server.js
@@ -12,7 +12,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/generate", generateRoute);
+const path = require("path");
+app.use(express.static(path.join(__dirname, "../client")));
 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/builder.html"));
+});
